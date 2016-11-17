7cfa89f5283e30eb5729d19566aa0db4a2bd4aa4
김지현 <gus5862@gmail.com>
2016-11-17 17:28:39 +0900

    회원 관련 처음 pull 했음

---
+++ Project/Fortune/src/main/java/com/fortune/Table_DTO/Join_DTO.java
@@ -0,0 +1,84 @@
+package com.fortune.Table_DTO;
+
+public class Join_DTO {
+	private String id;
+	private String pwd;
+	private String name; 
+	private String gender;
+	private String birthday;
+	private String phone;
+	private String enterdate;
+	private int deptno;
+	private int jobnum;
+	private int teamnum;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEnterdate() {
+		return enterdate;
+	}
+	public void setEnterdate(String enterdate) {
+		this.enterdate = enterdate;
+	}
+	public int getJobnum() {
+		return jobnum;
+	}
+	public void setJobnum(int jobnum) {
+		this.jobnum = jobnum;
+	}
+	public int getTeamnum() {
+		return teamnum;
+	}
+	public void setTeamnum(int teamnum) {
+		this.teamnum = teamnum;
+	}
+
+	public int getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+	@Override
+	public String toString() {
+		return "JoinDTO [id=" + id + ", pwd=" + pwd + ", name=" + name + ", gender=" + gender + ", birthday=" + birthday
+				+ ", phone=" + phone + ", enterdate=" + enterdate + ", deptno=" + deptno + ", jobnum=" + jobnum
+				+ ", teamnum=" + teamnum + ", toString()=" + super.toString() + "]";
+	}
+
+	
+}
