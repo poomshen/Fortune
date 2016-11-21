c5f461f4718822cfb1ed31218fc40af9bd6a0eb9
이명철 <fdi1234@gmail.com>
2016-11-21 11:06:02 +0900

    켈린더 삭제버튼 DB작업 연동완료

--- Project/Fortune/src/main/java/com/fortune/fullcalendar_DAO/IFullCalendar.java
+++ Project/Fortune/src/main/java/com/fortune/fullcalendar_DAO/IFullCalendar.java
@@ -3,14 +3,20 @@ package com.fortune.fullcalendar_DAO;
 import java.sql.SQLException;
 import java.util.List;
 
+import com.fortune.Table_DTO.Schedule_DTO;
 import com.fortune.function_DTO.Schedule_Work_DTO;
 
 public interface IFullCalendar {
 	//일정등록(schedule + work) 등록
 	public int insertSchedule(Schedule_Work_DTO swdto) throws ClassNotFoundException, SQLException;
 	public int insertWork(Schedule_Work_DTO swdto) throws ClassNotFoundException, SQLException;
+	public Schedule_DTO selectScheduleno() throws ClassNotFoundException, SQLException;
 	
 	//모든 일정내용 호출
 	public List<Schedule_Work_DTO> selectSWList() throws ClassNotFoundException, SQLException;
+	
+	//일정 삭제
+	public int deleteWork(String id) throws ClassNotFoundException, SQLException;
+	public int deleteSchedule(String id) throws ClassNotFoundException, SQLException;
     
 }
