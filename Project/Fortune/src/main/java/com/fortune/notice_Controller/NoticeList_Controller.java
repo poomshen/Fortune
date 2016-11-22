b98e7e5f3d838ed88c263b40b44b47ad43dfbc97
Jiyul-PC\Jiyul <s2jiyul@naver.com>
2016-11-22 18:20:58 +0900

    페이징처리까지 완료!!!!!

--- Project/Fortune/src/main/java/com/fortune/notice_Controller/NoticeList_Controller.java
+++ Project/Fortune/src/main/java/com/fortune/notice_Controller/NoticeList_Controller.java
@@ -13,8 +13,8 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.fortune.Table_DTO.Notice_DTO;
 import com.fortune.notice_DAO.INotice;
@@ -27,8 +27,51 @@ public class NoticeList_Controller {
 
 	// 공지사항 글목록보기
 	@RequestMapping("/noticeList.htm")
-	public String noticeList(String pg, String f, String q, Model model) throws ClassNotFoundException, SQLException {
+	public ModelAndView noticeList(String pg) throws ClassNotFoundException, SQLException {
+	
+		System.out.println("NoticeController의 noticeList를 탑니다~");
+		
+		INotice noticeListDao = sqlSession.getMapper(INotice.class);
+		
+		int page = 1;
+		String str_pg = pg;
+		if (str_pg != null) {
+			page = Integer.parseInt(str_pg);
+		}
+		int row_size = 9;
+		
+		int total_count = noticeListDao.CountNotice(); //공지사항 글 개수
+		System.out.println("total_count : " + total_count);
+		
+		//공지사항 글 목록
+		int all_page = (int)Math.ceil(total_count / (double)row_size); //페이지수
+		// int totalPage = total/rowSize + (total%rowSize==0?0:1);
+		System.out.println("페이지수 : " + all_page);
 
+		int block = 5; // 한페이지에 보여줄 범위 << [1] [2] [3] [4] [5] [6] [7] [8] [9]
+		// [10] >>
+		int from_page = ((page - 1) / block * block) + 1; // 보여줄 페이지의 시작
+		// ((1-1)/10*10)
+		int to_page = ((page - 1) / block * block) + block; // 보여줄 페이지의 끝
+		if (to_page > all_page) { // 예) 20>17
+			to_page = all_page;
+		}
+
+		List<Notice_DTO> noticelist = noticeListDao.listNotice(page);	//공지사항 리스트
+		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("noticelist", noticelist);
+		mv.addObject("total_count", total_count);
+		mv.addObject("pg", page);
+		mv.addObject("all_page", all_page);
+		mv.addObject("block", block);
+		mv.addObject("from_page", from_page);
+		mv.addObject("to_page", to_page);
+		mv.setViewName("notice/noticeList");
+		
+		return mv;
+
+/*
 		// 게시판 기본 설정(기본값 처리)/////////////
 		int page = 1;
 		String field = "NOTICE_TITLE";
@@ -46,13 +89,13 @@ public class NoticeList_Controller {
 
 		System.out.println(page + " / " + field + " / " + query);
 
-		System.out.println("NoticeController의 noticeList를 탑니다~");
+		
 
 		INotice noticeDao = sqlSession.getMapper(INotice.class);
 		List<Notice_DTO> noticeList = noticeDao.listNotice(page, field, query);
 		model.addAttribute("noticeList", noticeList);
 
-		return "notice/noticeList";
+		return "notice/noticeList";*/
 	}
 
 }
--- Project/Fortune/src/main/java/com/fortune/notice_DAO/INotice.java
+++ Project/Fortune/src/main/java/com/fortune/notice_DAO/INotice.java
@@ -1,5 +1,5 @@
 /*
-* @Class : NoticeAction
+* @Class : INotice
 * @Date : 2016.11.15
 * @Author : 김지율
 * @Desc : 공지사항게시판 인터페이스
@@ -9,15 +9,20 @@ package com.fortune.notice_DAO;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.ui.Model;
 
 import com.fortune.Table_DTO.Notice_DTO;
 
 public interface INotice {
-	
-	//게시물 개수
-	public int getCountNotice(String field, String query) throws ClassNotFoundException, SQLException;
+
+	//공지글 개수
+	public int CountNotice() throws ClassNotFoundException, SQLException;
 	//전체 게시물
-	public List<Notice_DTO> listNotice(int page, String field, String query) throws ClassNotFoundException, SQLException;
+	public List<Notice_DTO> listNotice(int page) throws ClassNotFoundException, SQLException;
 	//게시물 삭제
 	public int deleteNotice(String notice_no) throws ClassNotFoundException, SQLException;
 	//게시물 수정
@@ -28,9 +33,10 @@ public interface INotice {
 	public int insertNotice(Notice_DTO ndto) throws ClassNotFoundException, SQLException;
 	//공지사항 파일다운로드
 	public Notice_DTO fileDownloadNotice(String notice_no) throws ClassNotFoundException, SQLException;
-	
-	
-	/*	//조회수
+	/*	
+	//조회수
 	public void upHit(String notice_no) throws ClassNotFoundException, SQLException;*/
+	
+	
 
 }
