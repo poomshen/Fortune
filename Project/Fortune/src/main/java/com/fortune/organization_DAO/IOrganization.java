1f8b0759c824ba871748b2ac78bbdee6173541e8
김지현 <gus5862@gmail.com>
2016-11-29 01:12:07 +0900

    조직도 부서명 선택하면 팀명도 자동으로 바뀌게

--- Project/Fortune/src/main/java/com/fortune/organization_DAO/IOrganization.java
+++ Project/Fortune/src/main/java/com/fortune/organization_DAO/IOrganization.java
@@ -3,8 +3,10 @@ package com.fortune.organization_DAO;
 import java.util.ArrayList;
 
 import com.fortune.Table_DTO.Join_DTO;
+import com.fortune.Table_DTO.Team_DTO;
 
 public interface IOrganization {
 
-	public ArrayList<Join_DTO> showdept(int dept_no);
+	public ArrayList<Join_DTO> showdept(int dept_no, int team_no);
+	public ArrayList<Team_DTO> showteam(int dept_no);
 }
